8792f5233113f583363c7255f4c5ac520bbdda39
IsmaCoro <ismaelcm18182@gmail.com>
2024-11-20 16:32:48 -0600

    Juas

--- app.py
+++ app.py
@@ -251,7 +251,8 @@ def ver_solicitudes():
     return render_template('ver_solicitudes.html', solicitudes=solicitudes)
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
